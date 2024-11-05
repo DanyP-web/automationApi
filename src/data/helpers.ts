c5c56d8c95aa77092dcfc3f4b25aea3492962b26
User <user@Users-MacBook-Pro.local>
2024-11-04 22:01:12 -0500

    login2 spec created

--- src/data/helpers.ts
+++ src/data/helpers.ts
@@ -2,9 +2,9 @@ import * as supertest from 'supertest';
 import * as assert from "node:assert";
 const request = supertest("localhost:8001/api/v1")
 
-// export async function signUp(user: string | object | undefined){
-//   return await request.post("/users/signup").send(user)
-// } //simple
+export async function signUp2(user: string | object | undefined){
+  return await request.post("/users/signup").send(user)
+} //simple
 
 export function signUp(user:object):Promise<any>{
     return new Promise((resolve,reject) => {
--- src/specs/user/logIn.spec.ts
+++ src/specs/user/logIn.spec.ts
@@ -1,11 +1,12 @@
 import * as supertest from 'supertest';
 import {getUser} from "../../data/user"
-import {signUp, logIn} from "../../data/helpers";
-const request =supertest("localhost:8001/api/v1")
+import {signUp, signUp2, logIn} from "../../data/helpers";
+const request = supertest("localhost:8001/api/v1")
 
 describe('POSITIVE TESTING', () => {
     describe('LOGIN', () => {
         let userImport = getUser();
+
         it('login user', async() => {
            // const res = await request.post("/users/signup").send(userImport).expect(201)
             const res = await signUp(userImport)
@@ -20,6 +21,52 @@ describe('POSITIVE TESTING', () => {
                 password: userImport.password,
             })
         })
+        it('login user option 2', async() => {
+            await signUp(userImport).then((el)=>{
+                expect(el.body.status).toBe('success')
+                console.log(el.body,'res')
+            })
+            await logIn({
+                email: userImport.email,
+                password: userImport.password,
+            }).then(el2=> {
+                expect(el2.body.status).toBe('success')
+            })
+        })
+        it('login user option 3 using try and catch', async() => {
+            try{
+                await signUp(userImport).then((el)=>{
+                    expect(el.body.status).toBe('success')
+                    console.log(el.body,'res')
+                })
+                await logIn({
+                    email: userImport.email,
+                    password: userImport.password,
+                }).then(el2=> {
+                    expect(el2.body.status).toBe('success')
+                })
+            } catch (error){
+                console.log('Error during logIn process',error)
+                //throw Error(error)
+            }
+        })
+        it('login user option 4 using then', async() => {
+            signUp(userImport)
+                .then((res)=>{
+                    expect(res.body.status).toBe('success')
+                    return logIn({
+                        email: userImport.email,
+                        password: userImport.password,
+                    })
+                })
+                .then((res2)=>{
+                    expect(res2.statusCode).toBe(201)
+                })
+                .catch(err=>{
+                    console.log(err)
+                })
+        })
+
 
 
         })
